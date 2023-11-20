05bd1ddc8ae1efe2470a0a0e1c94efff9c0396ab
Raen Reyes <raen.reyes11@outlook.com>
2023-11-20 15:44:40 -0800

    first class

---
+++ OOP/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Car
+    {   //Default constructor
+        public Car() {
+            Console.WriteLine("Car was created");
+        }
+        public void Drive ()
+        {
+            Console.WriteLine("Car is driving");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("The car is now stopped!");
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        //A class in C# is a blue print for creating objects
+        //It defines a set of properties (data attributes)
+        // and methods that an object of that class will have
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   
+            //creating an instance of car in the main method called audi
+            Car audi = new Car();
+            //this is how can access a method from another class
+            audi.Drive();
+            Car bmw = new Car();//creating another instance of car
+            audi.Stop();
+            Console.WriteLine("Press 1 if you want the car to stop");
+
+            string userInput = Console.ReadLine();
+            if (userInput.Equals("1"))
+            {
+                audi.Stop();
+            }
+            else {
+                Console.WriteLine("Car keeps driving");
+            }
+            
         }
     }
 }
