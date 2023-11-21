1891d984e047a21eacf5de874a3642c1f3831892
Raen Reyes <raen.reyes11@outlook.com>
2023-11-20 18:07:06 -0800

    Constructors and Member Variables

--- OOP/Car.cs
+++ OOP/Car.cs
@@ -7,17 +7,33 @@ using System.Threading.Tasks;
 namespace OOP
 {
     internal class Car
-    {   //Default constructor
-        public Car() {
-            Console.WriteLine("Car was created");
+    {   //underscore is good way on knowing its private
+        //Member Variables 
+        private string _name; //private field to store data
+        private int _hp;
+        private string _color;
+        //Default constructor
+        public Car(string name, int hp = 0, string color = "black")
+        {
+            _name = name; //assign it to out private field
+            _hp = hp;
+            _color = color;
+            Console.WriteLine(name + " was created");
+            
+            
         }
+        //Member Methods
         public void Drive ()
         {
-            Console.WriteLine("Car is driving");
+            Console.WriteLine(_name + " is driving");
         }
         public void Stop()
         {
-            Console.WriteLine("The car is now stopped!");
+            Console.WriteLine(_name + " now stopped!");
+        }
+        public void Details ()
+        {
+            Console.WriteLine("The " + _color + " " + _name + " has an hp of " + _hp);
         }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,17 +2,18 @@
 {
     internal class Program
     {
-        //A class in C# is a blue print for creating objects
-        //It defines a set of properties (data attributes)
-        // and methods that an object of that class will have
+       
         static void Main(string[] args)
         {   
             //creating an instance of car in the main method called audi
-            Car audi = new Car();
+            Car audi = new Car("Audi A4",760,"Blue");
             //this is how can access a method from another class
+            audi.Details();
             audi.Drive();
-            Car bmw = new Car();//creating another instance of car
-            audi.Stop();
+            Car bmw = new Car("BMW M5",350,"Yellow");//creating another instance of car
+            bmw.Details();
+            bmw.Drive();
+
             Console.WriteLine("Press 1 if you want the car to stop");
 
             string userInput = Console.ReadLine();
